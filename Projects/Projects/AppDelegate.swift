d6639991d2fcef7f0d801291ed5d3977080b5ee6
pnarendran23 <pnarendran@usgbc.org>
2018-11-10 01:39:50 +0530

    Recent changes

--- Projects/Projects/AppDelegate.swift
+++ Projects/Projects/AppDelegate.swift
@@ -14,7 +14,7 @@ import Fabric
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-let mapKey = "<key>"
+let mapKey = "AIzaSyAioRmz1p523z5qLShx8f_l7Ueaq2EHcK8"
     var window: UIWindow?
 
 
